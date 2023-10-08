d05359e085419ecdbea28e9302ddaecfd43d2933
ketan0619 <gargoteketan619@gmail.com>
2023-10-08 12:13:37 +0530

    first commit

---
+++ assignment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "enter number n1 n2 n3 n4"
+read n1
+read n2
+read n3
+read n4
+addition=$[$n1+$n2+$n3+$n4]
+if mod=$[$addition/2]=0
+then
+	echo "addition is even"
+else 
+echo "addition is odd"
+fi
+
